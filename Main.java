80b1eb0fe0f82784c26a466c6a079a748b272132
EvergreenSchool <92188421+Evergree-Coded@users.noreply.github.com>
2021-11-20 00:40:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    for(int i = 1; i < 5; i++){
+      System.out.print(i + " ");
+    }     
+    //outputs: 1 2 3 4 
+
+    System.out.println(); 
+    
+    //Task1 : 
+    // outputs: 20 19 18 17 16 15 
+    // Hint: i--, will subtract 1
+    for(int i = 20; i > 14; i--){
+      System.out.print(i + " "); 
+    } 
+  }
+}
